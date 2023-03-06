e3b3c191e38310807fa3cc2373532c161e27e944
Pablo M <jpmora123@yahoo.com>
2023-03-06 09:17:20 -0500

    Diet Playlist V1

---
+++ app.py
@@ -0,0 +1,237 @@
+import json
+import requests
+import os
+from flask import Flask, render_template, request, flash, redirect, session, g
+from flask_debugtoolbar import DebugToolbarExtension
+# from wtforms_sqlalchemy.fields import QuerySelectField
+
+from sqlalchemy.exc import IntegrityError
+
+from forms import CreateUserForm, LoginForm, MakeOwnDietPlanForm
+from models import db, connect_db, User, Food, Diets
+
+CURR_USER_KEY = "curr_user"
+app = Flask(__name__)
+app.app_context().push()
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///diet_playlist'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "ManchesterUnited")
+app.debug = True
+toolbar = DebugToolbarExtension(app)
+connect_db(app)
+
+db.create_all()
+
+
+
+food_data_list = []
+DEFAULT_IMAGE = "https://media.istockphoto.com/id/1190330112/photo/fried-pork-and-vegetables-on-white-background.jpg?s=612x612&w=0&k=20&c=TzvLLGGvPAmxhKJ6fz91UGek-zLNNCh4iq7MVWLnFwo="
+app.config['SECRET_KEY'] = "Manchester United"
+
+
+
+@app.before_request
+def add_user_to_global():
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+    
+    else:
+        g.user = None
+
+def user_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+def user_logout():
+    "Logout user."
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+def get_food_data(ingredient):
+    food_data_list.clear()
+    app_id = '87acc9c6'
+    app_key = '18926addc269b99d7f700292aefdcf5d'
+    
+    url = 'https://api.edamam.com/api/food-database/parser?ingr=' + ingredient + '&app_id=' + app_id + '&app_key=' + app_key
+    response = requests.get(url)
+    return response.json()
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup."""
+
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+    form = CreateUserForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+                firstname=form.firstname.data,
+                lastname=form.lastname.data
+            )
+            
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already exist.", 'danger')
+            return render_template('users/signup.html', form=form)
+        
+        user_login(user)
+
+        return redirect("/")
+    
+    else: 
+        return render_template('users/signup.html', form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login"""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                form.password.data)
+            
+        if user:
+            user_login(user)
+            flash(f"Welcome, {user.username}", 'success')
+            return redirect("/")
+        flash("Invalid password", 'danger')
+
+    return render_template('users/login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    """Handle logout user."""
+
+    user_logout()
+    flash("Success log out!", 'success')
+
+    return redirect("/login") 
+
+
+
+
+@app.route('/search_food', methods=['GET', 'POST'])
+def food_search():
+    # Searching for food
+    if g.user is None or not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/login")
+    
+    else:
+        ingredient = request.args.get('q')
+        data = get_food_data(ingredient)
+
+        '''save the api data to an array and show on the template'''
+        for a_hints in data['hints']:
+            '''get the image_url or return default image'''
+            image_url = a_hints['food'].get('image') if a_hints['food'].get('image') else DEFAULT_IMAGE
+
+            '''get the nutrition list and return a single item in nutrition'''
+            nutritions = a_hints['food'].get('nutrients')
+            nutrition_list = []
+            for nutrient_name, nutrient_value in nutritions.items():
+                nutrition_list.append({'name': nutrient_name, 'value': round(nutrient_value, 2)})
+
+            food_data_list.append({'api_id': a_hints['food'].get('foodId'),
+                            'label': a_hints['food'].get('label'),
+                            'image_url': image_url,
+                            'nutrition': nutrition_list})
+            
+            user_id = g.user.id
+
+    # food_likes = [food.api_id for food in User.query.get(user_id).foods]
+    
+    return render_template('search_food.html', food_list=food_data_list, user_id= user_id)
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def meals():
+    form = MakeOwnDietPlanForm()
+
+    if form.validate_on_submit():
+        diet_name = request.form['diet_name']
+        diet_type = request.form['diet_type']
+        user_id=g.user.id
+        new_diet = Diets(diet_name=diet_name, diet_type=diet_type, user_id=user_id)
+        db.session.add(new_diet)
+        db.session.commit()
+
+        result = request.form
+        flash('New diet created successfully!', 'success')
+        return redirect('display_diet.html', diet_name=diet_name, diet_type=diet_type, result=result)
+
+    return render_template('create_diet.html', form=form)
+
+
+@app.route('/display_diets/<int:user_id>')
+def display_diets(user_id):
+    user_diets = Diets.query.filter_by(user_id=user_id).all()
+    result = request.form
+
+    return render_template('display_diets.html', diets=user_diets, result=result)
+
+
+
+# @app.route('/create_diet', methods=['GET, POST'])
+# def create_diet():
+#     form= MakeOwnDietPlanForm()
+#     diet_name = form.diet_name.data
+#     diet_type = form.diet_type.data
+#     # user_id = request.json.get('userId')
+#     new_diet = Diets(diet_name=diet_name, diet_type=diet_type)
+#     db.session.add(new_diet)
+#     db.session.commit()
+
+@app.route('/display_diets', methods=['GET', 'POST'])
+def show_user_diets():
+    form = MakeOwnDietPlanForm()
+    diet_name = form.diet_name.data
+    diet_type = form.diet_type.data
+    result = request.form
+    # api_id = request.json.get('apiId')
+    # user_id = request.json.get('userId')
+    # new_diet = Diets(diet_name=diet_name, diet_type=diet_type, user_id=user_id)
+    # db.session.add(new_diet)
+    # db.session.commit()
+
+    return render_template('display_diets.html', form=form, result=result, diet_name=diet_name, diet_type=diet_type)
+
+ 
+
+    
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """404 NOT FOUND page"""
+
+    return render_template('404.html'),  404
+
+ 
+
+@app.after_request
+def add_header(req):
+    """Add non-caching headers on every request."""
+
+    req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    req.headers["Pragma"] = "no-cache"
+    req.headers["Expires"] = "0"
+    req.headers['Cache-Control'] = 'public, max-age=0'
+    return req
---
+++ forms.py
@@ -0,0 +1,44 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, PasswordField, SelectField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class CreateUserForm(FlaskForm):
+    """form to create a users."""
+
+    username = StringField('Username', 
+        validators=[DataRequired()])
+    password = PasswordField('Password', 
+        validators=[DataRequired(), Length(min=6)])
+    email = StringField('E-mail',
+        validators=[DataRequired(), Email()])
+    firstname = StringField('First Name',
+        validators=[DataRequired()])
+    lastname = StringField('Last Name',
+        validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', 
+        validators=[DataRequired()])
+    password = PasswordField('Password', 
+        validators=[Length(min=6)])
+
+class FoodSearchForm(FlaskForm):
+    "food search form, user inputs ingredients and qty"
+
+    food = StringField('Name',
+        validators=[DataRequired()])
+    quantity = IntegerField('Quantity',
+        validators=[DataRequired()])
+
+# class MealTypeForm(FlaskForm):
+#     meal_type = SelectField(
+#         'Meal Type',  
+#         choices= [('breakfast', 'Breakfast'), ('lunch', 'Lunch'), ('dinner', 'Dinner'), ('snack', 'Snack')])
+
+class MakeOwnDietPlanForm(FlaskForm):
+    diet_name = StringField('Diet Name', validators=[DataRequired()])
+    diet_type = SelectField('Type of Diet', 
+    choices= [('weight loss', 'Weight Loss'), ('bulking', 'Bulking'), ('maintain', 'Maintain')])
