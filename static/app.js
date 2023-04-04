1932c3b801180b8b4fc3a72b0f63359b1cea3537
Pablo M <jpmora123@yahoo.com>
2023-04-03 20:39:23 -0400

    Updated styling

--- static/app.js
+++ static/app.js
@@ -101,52 +101,54 @@ table.addEventListener("click", (event) => {
     })
 
 
+// use put because you're updating 
+const form = document.querySelector("user_diets");
 
-// const form = document.querySelector("user_diets");
-
-// form.addEventListener("click", (event) => {
-//   if (event.target.classList.contains("submitbutton")) {
-//     const id = event.target.dataset.id;
-//     const diet_value = document.getElementById('diet_options').value;
-//     axios
-//       .get(`/search_food`, diet_value)
-//       .then((response) => {
-//         console.log('diet value sent successfully')
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         alert("Error deleting data.");
-//       });
-//   }
-// });
-
-const $ = document;
-let container = $.getElementsByClassName("cont")[0];
-
-const random = (min, max) => Math.floor(Math.random() * max + min);
-
-const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
-
-let drops = [];
-
-const snow = () => {
-  let w = random(1, 15),
-    op = Math.random(),
-    lef = random(0, 100),
-    del = random(0, 15);
-  sec = random(5, 15);
-  let drop = `<div style="position: absolute; width: ${w}px; height: ${w}px; opacity: ${op}; top: -2rem; left: ${lef}rem;animation: snow ${sec}s ${del}s linear; border-radius: 50%;z-index: 1000;filter: drop-shadow(0 0 10px white);background-color: #fff"></div>`;
-
-  drops.push(drop);
-};
-
-const main = async () => {
-  while (true) {
-    for (let i = 0; i < random(50, 100); i++) snow();
-    for (let i = 0; i < drops.length; i++) container.innerHTML += drops[i];
-    await sleep(30000);
-    container.innerHTML = "";
+form.addEventListener("click", (event) => {
+  if (event.target.classList.contains("deletefoodbutton")) {
+    const id = event.target.dataset.id;
+    const diet_value = document.getElementById('diet_options').value;
+    console.log(diet_value)
+    console.log(id)
+    axios
+      .get(`/diets/${}`, diet_value)
+      .then((response) => {
+        console.log('diet value sent successfully')
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Error deleting data.");
+      });
   }
-};
+});
 
-main();
+// const $ = document;
+// let container = $.getElementsByClassName("cont")[0];
+
+// const random = (min, max) => Math.floor(Math.random() * max + min);
+
+// const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+// let drops = [];
+
+// const snow = () => {
+//   let w = random(1, 15),
+//     op = Math.random(),
+//     lef = random(0, 100),
+//     del = random(0, 15);
+//   sec = random(5, 15);
+//   let drop = `<div style="position: absolute; width: ${w}px; height: ${w}px; opacity: ${op}; top: -2rem; left: ${lef}rem;animation: snow ${sec}s ${del}s linear; border-radius: 50%;z-index: 1000;filter: drop-shadow(0 0 10px white);background-color: #fff"></div>`;
+
+//   drops.push(drop);
+// };
+
+// const main = async () => {
+//   while (true) {
+//     for (let i = 0; i < random(50, 100); i++) snow();
+//     for (let i = 0; i < drops.length; i++) container.innerHTML += drops[i];
+//     await sleep(30000);
+//     container.innerHTML = "";
+//   }
+// };
+
+// main();
